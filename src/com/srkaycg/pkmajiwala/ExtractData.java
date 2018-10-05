0a5ec782b9ce3d4ae373ed147e693e1ee4df73b7
Pratik Majiwala <pratik.majiwala@srkaycg.com>
2018-10-05 14:20:09 +0530

    Improvements

--- src/com/srkaycg/pkmajiwala/ExtractData.java
+++ src/com/srkaycg/pkmajiwala/ExtractData.java
@@ -5,18 +5,37 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.supercsv.io.CsvListWriter;
+import org.supercsv.io.ICsvListWriter;
+import org.supercsv.prefs.CsvPreference;
 
 public class ExtractData {
 
 	static String PATH_TO_FEATURE = "src/com/srkaycg/pkmajiwala/add_cabin.feature";
 	static String PATH_TO_CSV = "src/com/srkaycg/pkmajiwala/data.csv";
 
+	public static StringWriter writeHashMapToCsv(HashMap<Integer, String> map) throws Exception {
+
+	    StringWriter output = new StringWriter();
+	    try (ICsvListWriter listWriter = new CsvListWriter(output, 
+	         CsvPreference.STANDARD_PREFERENCE)){
+	    	listWriter.write(map.get(1),map.get(2),map.get(3),map.get(4),map.get(5));
+	    }
+
+	   return output;
+	}
+	
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(PATH_TO_FEATURE));
 		BufferedWriter brOutput = new BufferedWriter(new FileWriter(PATH_TO_CSV));
+		ArrayList<HashMap<Integer, String>> outputData = new ArrayList<HashMap<Integer, String>>();
 		String line = new String();
 		String[] feature = new String[100];
 		String[] scenario = new String[100];
@@ -36,23 +55,23 @@ public class ExtractData {
 			if (line.contains("# ")) {
 				flag = 0;
 				scenarioid[++i] += line.replace("# ", "") + "\n";
-				System.out.println("Comment"+line);
+				System.out.println("Comment" + line);
 			}
 			if (line.contains("Scenario: ")) {
 				scenario[i] += line.replace(",", "-").replace("Scenario: ", "") + "\n";
-				System.out.println("scenario"+line);
+				System.out.println("scenario" + line);
 			}
 			if (line.contains("Scenario Outline: ")) {
 				scenario[i] += line.replace(",", "-").replace("Scenario Outline: ", "") + "\n";
-				System.out.println("Scenario Outline"+line);
+				System.out.println("Scenario Outline" + line);
 			}
 			if (line.contains("Given ")) {
 				given[i] += line.replace(",", "-") + "\n";
-				System.out.println("Given"+line.replace(",", "-"));
+				System.out.println("Given" + line.replace(",", "-"));
 			}
 			if (line.contains("When ")) {
 				when[i] += line.replace(",", "-") + "\n";
-				System.out.println("When"+line.replace(",", "-"));
+				System.out.println("When" + line.replace(",", "-"));
 			}
 			if (line.contains("And ")) {
 				if (flag == 1) {
@@ -60,25 +79,51 @@ public class ExtractData {
 				} else {
 					when[i] += line.replace(",", "-") + "\n";
 				}
-				System.out.println("And"+line);
+				System.out.println("And" + line);
 			}
 			if (line.contains("Then ")) {
 				then[i] += line.replace(",", "-") + "\n";
-				System.out.println("Then"+line);
+				System.out.println("Then" + line);
 				flag = 1;
 			}
 		}
+		System.out.println("----------------MAp" + "--------------------------");
+		HashMap<Integer, String> map = new HashMap<>();
 		for (int j = 0; j <= i; j++) {
 			try {
-			brOutput.write("\n"+scenarioid[j].trim().replace("null", "") + ",");
-			brOutput.write(scenario[j].trim().replace("null", "") + ",");
-			brOutput.write(given[j].trim().replace("null", "") + ",");
-			brOutput.write(when[j].trim().replace("null", "").replace("\n", "\n,,,") + ",");
-			brOutput.write(then[j].trim().replace("null", "").replace("\n", "\n,,,,") + ",\n");
-			}catch(Exception e) {
-				continue;
+				map.put(1,scenarioid[j].trim().replace("null", ""));
+			} catch (Exception e) {
+				map.put(1,"");
 			}
-
+			try {
+				map.put(2, scenario[j].trim().replace("null", ""));
+			} catch (Exception e) {
+				map.put(2,"");
+			}
+			try {
+				map.put(3, given[j].trim().replace("null", ""));
+			} catch (Exception e) {
+				map.put(3,"");
+			}
+			try {
+				map.put(4, when[j].trim().replace("null", ""));
+			} catch (Exception e) {
+				map.put(4,"");
+			}
+			try {
+				map.put(5, then[j].trim().replace("null", ""));
+			} catch (Exception e) {
+				map.put(5,"");
+			}
+			System.out.println(map.toString());
+			try {
+				brOutput.write(writeHashMapToCsv(map).toString());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			map.clear();
+			
 		}
 		br.close();
 		brOutput.close();
